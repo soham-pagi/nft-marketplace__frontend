8ac9c776e808c16a8beb51f752e3801f2b49744e
tvnivaskar <tvnivaskar@oneshield.com>
2023-03-12 15:28:25 +0530

    Tanmay, HeroSection Not working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
-import Login from "./Login/Login";
+import "../styles/globals.css";
+import Footer from './Footer/Footer'
+import NavBar from "./NavBar/NavBar";
+// import HeroSection from "./HeroSection/HeroSection"
 
 const App = () => {
-    return <Login />
+    return <>
+    <NavBar/>
+    {/* <HeroSection/> */}
+    <Footer/>
+    </>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import Collection from './components/Collection/Collection'
+import Aboutus from './pages/aboutus'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><App/></div>,
+  },
+  {
+    path: "collection",
+    element: <Collection/>
+  },
+  {
+    path: "aboutus",
+    element: <Aboutus/>
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
