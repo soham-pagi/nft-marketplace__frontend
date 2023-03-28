09322f941591a9d0882bf3c5fed33134f6e4e7e2
Soham Pagi <paguisoham36@gmail.com>
2023-03-29 04:48:22 +0530

    Fix Start Your Search in HeroSection.jsx

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -28,9 +28,8 @@ const HeroSection = () => {
             your NTFs and sell them
           </p>
           <Button
-            btnName="Start your search"
-            // handleClick={() => router.push("/searchPage")}
-            // handleClick={() => navigate.push("/searchPage")}
+            btnName="Start Your Search"
+            handleClick={() => window.location.replace('/searchPage')}
           />
         </div>
         <div className={Style.heroSection_box_right}>
