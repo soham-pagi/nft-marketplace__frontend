f41d8454679e13df3109073db12f8af4d938c7b1
Sejal Naik <naiksejal13@gmail.com>
2023-04-19 10:57:28 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 // INTERNAL IMPORT
-import { HeroSection, Service } from '../components/componentsindex';
+import { HeroSection, Service, Collection } from '../components/componentsindex';
 
 const Home = () => {
   return (
     <div style= {{ marginTop: 95 }}>
       <HeroSection />
       <Service />
+      <Collection />     
     </div>
   )
 }
